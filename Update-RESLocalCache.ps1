a5ba55810803325eab48f587445ead3fab8c1d39
timoline <timoline@gmail.com>
2014-04-16 21:13:39 +0200

    removed foreach, this should work too :-)

--- Update-RESLocalCache.ps1
+++ Update-RESLocalCache.ps1
@@ -11,30 +11,31 @@ function Update-RESLocalCache
     [CmdletBinding()] 
     param 
     (
-        [Parameter(Position=0,Mandatory = $true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]		
-        [Alias("CN","__SERVER","IPAddress")]
-        [string[]]$ComputerName=""   
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipelineByPropertyName=$true, ValueFromPipeline=$true)]	
+        [Alias("CN","ComputerName","__SERVER")] 
+        [string]
+        $Server  
     )
 
-    foreach($c in $ComputerName)
-    {	
+    Process
+    {
         Write-Verbose 'Forcing a local cache update for RES Powerfuse / Workspace Manager.'
 
         $ResPath = (Get-RESRegistryPath).Substring(6) 
         $RegKey = "UpdateGUIDs"
 
-        $Registry = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $c ) 
+        $Registry = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine', $Server ) 
         $RegistryKey = $Registry.OpenSubKey($ResPath,$true)       
         $RegistryKey.DeleteSubKeyTree($RegKey)
         Write-Verbose "Deleting key."
 
         #stop, wait and start the service
-        (Get-Service -ComputerName $c | where {$_.name -eq "RES"}).stop()
-        (Get-Service -ComputerName $c | where {$_.name -eq "RES"}).WaitForStatus("Stopped")
+        (Get-Service -ComputerName $Server | where {$_.name -eq "RES"}).stop()
+        (Get-Service -ComputerName $Server | where {$_.name -eq "RES"}).WaitForStatus("Stopped")
         Write-Verbose 'RES Service Stopped.'        
         
-        (Get-Service -ComputerName $c | where {$_.name -eq "RES"}).start()
-        (Get-Service -ComputerName $c | where {$_.name -eq "RES"}).WaitForStatus("Running")
+        (Get-Service -ComputerName $Server | where {$_.name -eq "RES"}).start()
+        (Get-Service -ComputerName $Server | where {$_.name -eq "RES"}).WaitForStatus("Running")
         Write-Verbose 'RES Service Started.'
     
         Write-Verbose 'Local cache update for RES Powerfuse / Workspace Manager is succefully forced.'
