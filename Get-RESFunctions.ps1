bbdddd6b615ad6cb6c7a35047d788388dfbd656d
timoline <timoline@gmail.com>
2015-08-08 09:11:45 +0200

    addes 2 new cmdlets for internal module use : Get-RESPowerzoneName, Get-RESWorkspaceName

--- Get-RESFunctions.ps1
+++ Get-RESFunctions.ps1
@@ -25,3 +25,31 @@
         }  
     }#process 
 }
+
+Filter Get-RESWorkspaceName
+{
+    [CmdletBinding()] 
+    param ( 
+        # The guid - pattern of the guid
+        [Parameter(Mandatory=$true)]
+        [string] 
+        $Guid       
+    )
+    #$Guid = "{E536D09E-3DA0-464A-84A5-F520C43C1429}"
+    $Value = Get-RESWorkspaces -Guid $Guid     
+    return $Value.name.ToString()
+}
+
+Filter Get-RESPowerzoneName
+{
+    [CmdletBinding()] 
+    param ( 
+        # The guid - pattern of the guid
+        [Parameter(Mandatory=$true)]
+        [string] 
+        $Guid       
+    )
+    #$Guid = "{EF0E53B1-B968-48EF-8089-C3ABB154E34F}"
+    $Value = Get-RESPowerzones -Guid $Guid 
+    return $Value.name.ToString()
+}
