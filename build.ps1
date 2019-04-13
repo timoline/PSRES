8e0ba973bb1b6d0d76d0431b13f2d3714ddb058d
timoline <timoline@gmail.com>
2019-04-13 12:40:06 +0200

    1st try builddocs

--- build.ps1
+++ build.ps1
@@ -1,3 +1,14 @@
+$moduleName = "PSRES"
+$modulePath = "out\$moduleName"
+$docPath = "$modulePath\docs" 
+$author = 'Timrz'
+$version = '0.0.1'
 
 Install-Module PlatyPS -Scope CurrentUser -Force -verbose
 Install-Module Pester -Scope CurrentUser -Force -verbose
+
+Import-Module $moduleName -Force -Verbose
+
+#New-MarkdownHelp -Module $moduleName -OutputFolder $docPath -WithModulePage -Force
+
+Update-MarkdownHelpModule  $docPath -RefreshModulePage -verbose
