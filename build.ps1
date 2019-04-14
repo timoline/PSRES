5d1470f2ac2737b92315dbabe7c7417debe1cec2
timoline <timoline@gmail.com>
2019-04-14 16:01:20 +0200

    2nd try

--- build.ps1
+++ build.ps1
@@ -8,12 +8,12 @@ Install-Module PlatyPS -Scope CurrentUser -Force -verbose
 Install-Module Pester -Scope CurrentUser -Force -verbose
 
 # copy artifacts
-New-Item -Type Directory out -ErrorAction SilentlyContinue > $null
-Copy-Item -Recurse -Force src out
+New-Item -Type Directory out -ErrorAction SilentlyContinue -Verbose
+Copy-Item -Recurse -Force src out -Verbose
 
 Remove-Module $moduleName -ErrorAction SilentlyContinue
-Import-Module $pwd\out\$moduleName -Force -Verbose
+#Import-Module $pwd\out\$moduleName -Force -Verbose
 
 #New-MarkdownHelp -Module $moduleName -OutputFolder $docPath -WithModulePage -Force
 
-Update-MarkdownHelpModule  $docPath -RefreshModulePage -verbose
+#Update-MarkdownHelpModule  $docPath -RefreshModulePage -verbose
