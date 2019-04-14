0fc4fd3a6b77cc4aaf06f3ffe339dd7fb25b042f
timoline <timoline@gmail.com>
2019-04-14 16:50:56 +0200

    revert all

--- build.ps1
+++ build.ps1
@@ -4,17 +4,5 @@ $docPath = "out\docs"
 $author = 'Timrz'
 $version = '0.0.1'
 
-Install-Module PlatyPS -Scope CurrentUser -Force -verbose
-Install-Module Pester -Scope CurrentUser -Force -verbose
-
-# copy artifacts
-New-Item -Type Directory out -ErrorAction SilentlyContinue -Verbose
-New-Item -Type Directory out\docs -ErrorAction SilentlyContinue -Verbose
-Copy-Item -Recurse -Force src out -Verbose
-
-Remove-Module $moduleName -ErrorAction SilentlyContinue
-Import-Module out\src\$moduleName.psd1 -Force -Verbose
-
-New-MarkdownHelp -Module $moduleName -OutputFolder $docPath -WithModulePage -Force
-
-Update-MarkdownHelpModule  $docPath -RefreshModulePage -verbose
+#Install-Module PlatyPS -Scope CurrentUser -Force -verbose
+#Install-Module Pester -Scope CurrentUser -Force -verbose
