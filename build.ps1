2c00e43387489a4e6ee8a6d3f2a280cfe2be034d
timoline <timoline@gmail.com>
2019-04-14 15:58:03 +0200

    letstry

--- build.ps1
+++ build.ps1
@@ -7,7 +7,12 @@ $version = '0.0.1'
 Install-Module PlatyPS -Scope CurrentUser -Force -verbose
 Install-Module Pester -Scope CurrentUser -Force -verbose
 
-Import-Module $moduleName -Force -Verbose
+# copy artifacts
+New-Item -Type Directory out -ErrorAction SilentlyContinue > $null
+Copy-Item -Recurse -Force src out
+
+Remove-Module $moduleName -ErrorAction SilentlyContinue
+Import-Module $pwd\out\$moduleName -Force -Verbose
 
 #New-MarkdownHelp -Module $moduleName -OutputFolder $docPath -WithModulePage -Force
 
