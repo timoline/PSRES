3e9efe3aaaffdfdeeb9182e57f86737186495a1f
timoline <timoline@gmail.com>
2019-04-15 20:25:43 +0200

    added some test

--- Tests/a.tests.ps1
+++ Tests/a.tests.ps1
@@ -1,1 +1,12 @@
 Import-Module "$PSScriptRoot/../src/PSRES.psd1" -Force
+
+describe 'first-test' {
+
+    $TestNumber = 2
+    $result = $TestNumber
+
+    it 'should return 2' {
+        $result | should be 2
+    }
+
+}
